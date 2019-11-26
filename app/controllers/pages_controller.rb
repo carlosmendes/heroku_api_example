fc4c8ef304ca8d6909c35546f865ad57dfb5caec
Carlos <carls.mendes@gmail.com>
2019-11-26 18:11:41 +0000

    tests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,11 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def create_app
+    heroku = PlatformAPI.connect_oauth(ENV['HEROKU_OAUTH_TOKEN'])
+    result = heroku.app.create({})
+
+    redirect_to root_path, notice: result['web_url']
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'create-app', to: 'pages#create_app'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
